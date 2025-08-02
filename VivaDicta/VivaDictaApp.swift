718bbdf64ac6deae2799fd052a8ce3e299419b89
Anton Novoselov <nantog@gmail.com>
2025-08-02 15:19:18 +0100

    Initial Commit

---
+++ VivaDicta/VivaDictaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VivaDictaApp.swift
+//  VivaDicta
+//
+//  Created by Anton Novoselov on 02.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct VivaDictaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
