e3ab7dd4340e19d49e3c1e5402755a397c83b46a
Anton Novoselov <nantog@gmail.com>
2025-08-05 08:42:39 +0100

    Cleanup

--- VivaDicta/AudioRecorder.swift
+++ VivaDicta/AudioRecorder.swift
@@ -31,7 +31,7 @@ class AudioRecorder {
     private let temporaryURL = URL.documentsDirectory.appending(path: "recording.m4a")
     
     private var audioPermissionStatus: AVAudioApplication.recordPermission = .undetermined
-   
+    
     
     
     func recordAudio() {
@@ -70,23 +70,23 @@ class AudioRecorder {
     }
     
     func stopRecording() {
-            audioRecorder?.stop()
-            
-            let id = UUID()
-            let filename = "\(id.uuidString).m4a"
-            
-            let fileURL = URL.documentsDirectory.appending(path: filename)
-            
-            let record = Record(name: "test", fileURL: fileURL.absoluteString)
-            do {
-                try FileManager.default.moveItem(at: temporaryURL, to: fileURL)
-                recordingState = .completed(record)
-            } catch {
-                print(error.localizedDescription)
-                recordingState = .idle
-            }
-            print(fileURL)
-
+        audioRecorder?.stop()
+        
+        let id = UUID()
+        let filename = "\(id.uuidString).m4a"
+        
+        let fileURL = URL.documentsDirectory.appending(path: filename)
+        
+        let record = Record(name: "test", fileURL: fileURL.absoluteString)
+        do {
+            try FileManager.default.moveItem(at: temporaryURL, to: fileURL)
+            recordingState = .completed(record)
+        } catch {
+            print(error.localizedDescription)
+            recordingState = .idle
+        }
+        print(fileURL)
+        
         
     }
     
