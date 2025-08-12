8709cfdac985dc3f15b0736e621c9c82456ede74
Anton Novoselov <nantog@gmail.com>
2025-08-12 20:39:05 +0100

    Add different languages support

--- VivaDicta/Whisper/LibWhisper.swift
+++ VivaDicta/Whisper/LibWhisper.swift
@@ -9,6 +9,10 @@ enum WhisperError: Error {
 // Meet Whisper C++ constraint: Don't access from more than one thread at a time.
 actor WhisperContext {
     private var context: OpaquePointer
+    private var languageCString: [CChar]?
+    private var prompt: String?
+    private var promptCString: [CChar]?
+    private var vadModelPath: String?
 
     init(context: OpaquePointer) {
         self.context = context
@@ -18,34 +22,62 @@ actor WhisperContext {
         whisper_free(context)
     }
 
-    func fullTranscribe(samples: [Float]) {
+    func fullTranscribe(samples: [Float]) -> Bool {
         // Leave 2 processors free (i.e. the high-efficiency cores).
         let maxThreads = max(1, min(8, ProcessInfo.processInfo.processorCount - 2))
         print("Selecting \(maxThreads) threads")
         var params = whisper_full_default_params(WHISPER_SAMPLING_GREEDY)
-        "en".withCString { en in
-            // Adapted from whisper.objc
-            params.print_realtime   = true
-            params.print_progress   = false
-            params.print_timestamps = true
-            params.print_special    = false
-            params.translate        = false
-            params.language         = en
-            params.n_threads        = Int32(maxThreads)
-            params.offset_ms        = 0
-            params.no_context       = true
-            params.single_segment   = false
-
-            whisper_reset_timings(context)
-            print("About to run whisper_full")
-            samples.withUnsafeBufferPointer { samples in
-                if (whisper_full(context, params, samples.baseAddress, Int32(samples.count)) != 0) {
-                    print("Failed to run the model")
-                } else {
-                    whisper_print_timings(context)
-                }
+        
+        // Read language directly from UserDefaults
+        let selectedLanguage = UserDefaults.standard.string(forKey: "SelectedLanguage") ?? "ru"
+        if selectedLanguage != "auto" {
+            languageCString = Array(selectedLanguage.utf8CString)
+            params.language = languageCString?.withUnsafeBufferPointer { ptr in
+                ptr.baseAddress
+            }
+        } else {
+            languageCString = nil
+            params.language = nil
+        }
+        
+        if true  || prompt != nil {
+            promptCString = Array("Здравствуйте, как ваши дела? Приятно познакомиться.".utf8CString)
+            params.initial_prompt = promptCString?.withUnsafeBufferPointer { ptr in
+                ptr.baseAddress
+            }
+        } else {
+            promptCString = nil
+            params.initial_prompt = nil
+        }
+        
+        
+        params.print_realtime = true
+        params.print_progress = false
+        params.print_timestamps = true
+        params.print_special = false
+        params.translate = false
+        params.n_threads = Int32(maxThreads)
+        params.offset_ms = 0
+        params.no_context = true
+        params.single_segment = false
+        params.temperature = 0.2
+        
+        whisper_reset_timings(context)
+        
+        var success = true
+        samples.withUnsafeBufferPointer { samplesBuffer in
+            if whisper_full(context, params, samplesBuffer.baseAddress, Int32(samplesBuffer.count)) != 0 {
+                print("Failed to run whisper_full.")
+                success = false
+            } else {
+                whisper_print_timings(context)
             }
         }
+        
+        languageCString = nil
+        promptCString = nil
+        
+        return success
     }
 
     func getTranscription() -> String {
@@ -71,5 +103,9 @@ actor WhisperContext {
             print("Couldn't load model at \(path)")
             throw WhisperError.couldNotInitializeContext
         }
+    }
+    
+    func setPrompt(_ prompt: String?) {
+        self.prompt = prompt
     }
 }
