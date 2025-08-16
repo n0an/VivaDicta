dc3d9b05d1303f118b543ad10f9ba6212e5dd129
Anton Novoselov <nantog@gmail.com>
2025-08-16 13:42:48 +0100

    Update UI

--- VivaDicta/Screens/ModelsScreen/ModelsView.swift
+++ VivaDicta/Screens/ModelsScreen/ModelsView.swift
@@ -11,30 +11,29 @@ struct ModelsView: View {
     @Bindable var appState: AppState
     
     var body: some View {
-        
         NavigationStack {
-            VStack {
-                Menu("Language", systemImage: "globe") {
-                    Picker("Language", selection: $appState.selectedLanguage) {
-                        ForEach(Language.allCases, id: \.self) { language in
-                            Text(language.fullName)
-                                .tag(language)
+            List {
+                NavigationLink("Cloud") {
+                    CloudModelsList()
+                }
+                NavigationLink("Local") {
+                    WhisperModelsList(appState: appState)
+                }
+            }
+            .navigationBarTitle("Models")
+            .toolbar {
+                ToolbarItem {
+                    Menu("Language", systemImage: "globe") {
+                        Picker("Language", selection: $appState.selectedLanguage) {
+                            ForEach(Language.allCases, id: \.self) { language in
+                                Text(language.fullName)
+                                    .tag(language)
+                            }
                         }
                     }
                 }
-                .padding(.trailing, 40)
-                .frame(maxWidth: .infinity, alignment: .trailing)
                 
-                List {
-                    NavigationLink("Cloud") {
-                        CloudModelsList()
-                    }
-                    NavigationLink("Local") {
-                        WhisperModelsList(appState: appState)
-                    }
-                }
             }
-            .navigationBarTitle("Models", displayMode: .inline)
         }
     }
 }
