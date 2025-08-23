9ef9b75ff86b694b44d73c9ba1f60b5b4c21f2d3
Anton Novoselov <nantog@gmail.com>
2025-08-23 08:49:18 +0100

    Add extensions

--- VivaDicta/Utils/Extensions.swift
+++ VivaDicta/Utils/Extensions.swift
@@ -163,3 +163,35 @@ extension BadgeStyle where Self == FancyBadgeStyle {
         FancyBadgeStyle()
     }
 }
+
+extension View {
+    @ViewBuilder
+    func debugBorder() -> some View {
+        #if DEBUG
+        self.border(Color.random())
+        #else
+        self
+        #endif
+    }
+    
+    @ViewBuilder
+    func iflet<Value>(_ value: Value?, @ViewBuilder transform: (Value, Self) -> some View) -> some View {
+        if let value {
+            transform(value, self)
+        } else {
+            self
+        }
+    }
+}
+
+extension Color {
+    static func random() -> Color {
+        Color(
+            red: Double.random(in: 0...1),
+            green: Double.random(in: 0...1),
+            blue: Double.random(in: 0...1)
+        )
+    }
+    
+    var sui: Color { Color(self) }
+}
