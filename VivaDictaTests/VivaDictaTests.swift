f423c08e75ea6129ae95734fc18f2fcab3318459
Anton Novoselov <nantog@gmail.com>
2025-08-30 16:28:24 +0100

    Add Unit tests target

---
+++ VivaDictaTests/VivaDictaTests.swift
@@ -0,0 +1,17 @@
+//
+//  VivaDictaTests.swift
+//  VivaDictaTests
+//
+//  Created by Anton Novoselov on 30.08.2025.
+//
+
+import Testing
+
+struct VivaDictaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions
+        #expect(true)
+    }
+
+}
