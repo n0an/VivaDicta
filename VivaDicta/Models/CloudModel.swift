b30c859dcc32b4c0564fa74363bcbe0ee0010195
Anton Novoselov <nantog@gmail.com>
2025-09-03 10:34:26 +0100

    Cleanup

---
+++ VivaDicta/Models/CloudModel.swift
@@ -0,0 +1,42 @@
+//
+//  CloudModel.swift
+//  VivaDicta
+//
+//  Created by Anton Novoselov on 2025.09.03
+//
+
+import Foundation
+
+struct CloudModel: @MainActor TranscriptionModel {
+    let id: UUID
+    let name: String
+    let displayName: String
+    let description: String
+    let provider: TranscriptionModelProvider
+    
+    let speed: Double
+    let accuracy: Double
+    let supportManyLanguages: Bool
+    let supportedLanguages: [String: String]
+    
+    init(id: UUID = UUID(),
+         name: String,
+         displayName: String,
+         description: String,
+         provider: TranscriptionModelProvider,
+         speed: Double,
+         accuracy: Double,
+         supportManyLanguages: Bool,
+         supportedLanguages: [String: String])
+    {
+        self.id = id
+        self.name = name
+        self.displayName = displayName
+        self.description = description
+        self.provider = provider
+        self.speed = speed
+        self.accuracy = accuracy
+        self.supportManyLanguages = supportManyLanguages
+        self.supportedLanguages = supportedLanguages
+    }
+}
--- VivaDicta/Models/TranscriptionModel.swift
+++ VivaDicta/Models/TranscriptionModel.swift
@@ -283,37 +283,3 @@ protocol TranscriptionModel: Identifiable, Hashable {
     var supportManyLanguages: Bool { get }
     var supportedLanguages: [String: String] { get }
 }
-
-struct CloudModel: @MainActor TranscriptionModel {
-    let id: UUID
-    let name: String
-    let displayName: String
-    let description: String
-    let provider: TranscriptionModelProvider
-    
-    let speed: Double
-    let accuracy: Double
-    let supportManyLanguages: Bool
-    let supportedLanguages: [String: String]
-    
-    init(id: UUID = UUID(),
-         name: String,
-         displayName: String,
-         description: String,
-         provider: TranscriptionModelProvider,
-         speed: Double,
-         accuracy: Double,
-         supportManyLanguages: Bool,
-         supportedLanguages: [String: String])
-    {
-        self.id = id
-        self.name = name
-        self.displayName = displayName
-        self.description = description
-        self.provider = provider
-        self.speed = speed
-        self.accuracy = accuracy
-        self.supportManyLanguages = supportManyLanguages
-        self.supportedLanguages = supportedLanguages
-    }
-}
