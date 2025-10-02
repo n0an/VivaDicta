7c41ec774a3dc7c0c383ccddab81378672563fbb
Anton Novoselov <nantog@gmail.com>
2025-10-02 11:23:01 +0100

    Add URLOpener to Keyboard

--- VivaDictaKeyboard/KeyboardViewController.swift
+++ VivaDictaKeyboard/KeyboardViewController.swift
@@ -164,3 +164,26 @@ class KeyboardViewController: KeyboardInputViewController {
     }
 
 }
+
+
+
+struct URLOpener {
+  let responder: UIResponder
+
+  func open(urlString: String) {
+    if let url = URL(string: urlString) {
+      var optionalResponder: UIResponder? = responder
+      let selector = NSSelectorFromString("openURL:")
+      while let responder = optionalResponder {
+        if responder.responds(to: selector) {
+          responder.perform(selector, with: url)
+          return
+        }
+        optionalResponder = responder.next
+      }
+      print("Can't open", urlString)
+    } else {
+      print("Can't open", urlString)
+    }
+  }
+}
