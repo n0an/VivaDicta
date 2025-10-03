96561a9477e0be310a11d6fabf35d0bd56990bbb
Anton Novoselov <nantog@gmail.com>
2025-10-03 10:36:20 +0100

    Add Darwin notification names and App Groups configuration to AppGroupCoordinator

--- VivaDicta/AppGroupCoordinator.swift
+++ VivaDicta/AppGroupCoordinator.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+// MARK: - App Groups Configuration
+public enum AppGroupConfig {
+    static let appGroupId = "group.com.antonnovoselov.VivaDicta"
+    static let heartbeatKey = "appLastHeartbeat"
+    static let isMainAppActiveKey = "isMainAppActive"
+    static let heartbeatInterval: TimeInterval = 5.0
+    static let heartbeatThreshold: TimeInterval = 10.0  // Consider app active if heartbeat is within 10 seconds
+}
+
+// MARK: - Darwin Notification Names
+public enum DarwinNotificationName {
+    /// Notification sent from keyboard to start recording
+    static let startRecording = "com.antonnovoselov.VivaDicta.startRecording"
+
+    /// Notification sent from keyboard to stop recording
+    static let stopRecording = "com.antonnovoselov.VivaDicta.stopRecording"
+
+    /// Notification sent from main app when transcription is ready
+    static let transcriptionReady = "com.antonnovoselov.VivaDicta.transcriptionReady"
+
+    /// Notification sent from main app when recording has started
+    static let recordingStarted = "com.antonnovoselov.VivaDicta.recordingStarted"
+
+    /// Notification sent from main app when recording has stopped
+    static let recordingStopped = "com.antonnovoselov.VivaDicta.recordingStopped"
+
+    /// Notification sent when an error occurs
+    static let recordingError = "com.antonnovoselov.VivaDicta.recordingError"
+}
+
+public final class AppGroupCoordinator {
+
+}
