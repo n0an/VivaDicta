b8c845f3b65d0114d5aa06f86c7b166592fa7b2a
Anton Novoselov <nantog@gmail.com>
2025-11-14 12:40:19 +0000

    Cleanup

--- VivaDicta/AppIntents/ToggleRecordIntent.swift
+++ VivaDicta/AppIntents/ToggleRecordIntent.swift
@@ -9,7 +9,6 @@ import AppIntents
 import SwiftUI
 
 struct ToggleRecordIntent: AppIntent {
-
     static let title: LocalizedStringResource = "Toggle Recording"
     static let description = IntentDescription("Toggle recording in VivaDicta")
 
@@ -17,27 +16,18 @@ struct ToggleRecordIntent: AppIntent {
 
     @available(iOS 26.0, *)
     static let supportedModes: IntentModes = .foreground(.immediate)
-
-
+    
     func perform() async throws -> some IntentResult {
-        // Check current recording state and toggle accordingly
         await MainActor.run {
             let coordinator = AppGroupCoordinator.shared
             let isCurrentlyRecording = coordinator.isRecording
 
             if isCurrentlyRecording {
-                // If recording, stop it
                 coordinator.requestStopRecording()
             } else {
-                // If not recording, start it from Control Center
                 coordinator.requestStartRecordingFromControl()
             }
         }
-
-        // The app will open due to openAppWhenRun = true
-        // and will receive the appropriate notification
-
         return .result()
     }
-
 }
--- VivaDictaWidget/VivaDictaWidgetControl.swift
+++ VivaDictaWidget/VivaDictaWidgetControl.swift
@@ -13,79 +13,12 @@ struct VivaDictaWidgetControl: ControlWidget {
     var body: some ControlWidgetConfiguration {
         
         StaticControlConfiguration(kind: "VivaDictaControlWidget") {
-            
             ControlWidgetButton(action: ToggleRecordIntent()) {
                 Image(systemName: "microphone.circle")
             }
         }
         
-        
         .displayName("Toggle Record")
         .description("Toggle Record in VivaDicta")
-    }
-}
-
-
-
-
-
-/// An App Intent that generates a developer horoscope based on a GitHub username.
-///
-/// This intent is used to trigger horoscope generation through system integrations
-/// such as Siri, Shortcuts, or Spotlight. It calls `HoroscopeService` to fetch
-/// the result and returns a `HoroscopeView` to display it.
-///
-/// - Parameters:
-///   - username: The GitHub username used to generate a personalized horoscope.
-/// - Returns: A rendered `HoroscopeView` inside a system snippet UI.
-//struct HoroscopeIntent: AppIntent {
-//    static var parameterSummary: some ParameterSummary {
-//        Summary("Generate a horoscope for \(\.$username)")
-//    }
-//
-//    static var title: LocalizedStringResource = "Horoscope"
-//    static var description = IntentDescription("Generates a horoscope")
-//
-//    @Parameter(title: "Github username")
-//    var username: String
-//
-//    @Dependency
-//    private var horoscopeService: HoroscopeService
-//
-//    func perform() async throws -> some IntentResult & ShowsSnippetView {
-//        let horoscope = try await horoscopeService.horoscope(username: username)
-//        return .result(view: HoroscopeView(horoscope: horoscope))
-//    }
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-struct StartTimerIntent: SetValueIntent {
-    static let title: LocalizedStringResource = "Start a timer"
-
-    @Parameter(title: "Timer Name")
-    var name: String
-
-    @Parameter(title: "Timer is running")
-    var value: Bool
-
-    init() {}
-
-    init(_ name: String) {
-        self.name = name
-    }
-
-    func perform() async throws -> some IntentResult {
-        // Start the timer…
-        return .result()
     }
 }
