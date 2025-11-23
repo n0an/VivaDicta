b88a169bcba15c7c0d356097c27991a0a9b57328
Anton Novoselov <nantog@gmail.com>
2025-11-23 16:08:37 +0000

    Update HudView UI

--- VivaDicta/Views/HudView.swift
+++ VivaDicta/Views/HudView.swift
@@ -29,28 +29,63 @@ struct HudView: View {
     
     @State var isSymbolAnimating = false
     
-    
-    
     var body: some View {
         
         VStack(spacing: 12) {
             Image(systemName: statusIcon)
-                .foregroundStyle(Color.blue)
                 .symbolEffect(.bounce.up.byLayer, options: .repeat(.periodic(delay: 0.3)), isActive: isSymbolAnimating)
                 .font(.system(size: 50, weight: .semibold))
-//                .frame(height: 50)
                 .onAppear { isSymbolAnimating = true }
                 .onDisappear { isSymbolAnimating = false }
             
-            // Processing status label
             Text(statusText)
                 .font(.system(size: 17, weight: .semibold))
-                .foregroundStyle(.primary)
                 .animation(.easeInOut(duration: 0.3), value: statusText)
         }
+        .foregroundColor(.white)
         .padding()
-        .background(.gray.opacity(0.2), in: .rect(cornerRadius: 20))
-        
+        .background(
+            ZStack {
+                AnimatedMeshGradient()
+                    .mask(
+                        RoundedRectangle(cornerRadius: 16, style: .continuous)
+                            .stroke(lineWidth: 16)
+                            .blur(radius: 8)
+                    )
+                
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(lineWidth: 3)
+                            .fill(Color.white)
+                            .blur(radius: 2)
+                            .blendMode(.overlay)
+                    )
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(lineWidth: 1)
+                            .fill(Color.white)
+                            .blur(radius: 1)
+                            .blendMode(.overlay)
+                    )
+                
+            }
+        )
+        .background(
+            ZStack {
+                AnimatedMeshGradient()
+                    .frame(width: 400, height: 800)
+                    .opacity(1)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+        )
+        .background(.black)
+        .clipShape(RoundedRectangle(cornerRadius: 16))
+        .background(
+            RoundedRectangle(cornerRadius: 16)
+                .stroke(.primary.opacity(0.5), lineWidth: 1)
+        )
+        .shadow(color: .black.opacity(0.15), radius: 20, x: 0, y: 20)
+        .shadow(color: .black.opacity(0.1), radius: 15, x: 0, y: 15)
     }
 }
 
