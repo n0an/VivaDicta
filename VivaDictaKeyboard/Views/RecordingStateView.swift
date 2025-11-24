32b5b7cce8cf408348038fbafc4c15b672ede4eb
Anton Novoselov <nantog@gmail.com>
2025-11-24 11:43:45 +0000

    Add AnimatedRectangle

--- VivaDictaKeyboard/Views/RecordingStateView.swift
+++ VivaDictaKeyboard/Views/RecordingStateView.swift
@@ -10,10 +10,34 @@ import KeyboardKit
 import SiriWaveView
 
 struct RecordingStateView: View {
+    
+    @State private var maskTimer: Float = 0.0
 
     @State var isSymbolAnimating = false
 
     @Bindable var dictationState: KeyboardDictationState
+    
+    @State var timer: Timer?
+
+    private var rectangleSpeed: Float {
+        
+        switch dictationState.uiState {
+        case .recording:
+            0.03
+        case .processing:
+            0.03
+        default:
+            0
+        
+        }
+        
+//        switch state {
+//        case .none: return 0
+//        case .thinking: return 0.03
+//        }
+    }
+    
+    
     
     
     var body: some View {
@@ -60,7 +84,28 @@ struct RecordingStateView: View {
                 .background(.red, in: .capsule)
             }
         }
-        
+        .overlay {
+            if dictationState.uiState == .recording || dictationState.uiState == .processing {
+                AnimatedMeshGradient()
+                    .mask(
+                        AnimatedRectangle(size: .init(width: 80, height: 80), cornerRadius: 20, t: CGFloat(maskTimer))
+                    )
+                    .frame(width: 80, height: 80)
+                    .ignoresSafeArea()
+            }
+        }
+        .onAppear {
+            timer = Timer.scheduledTimer(withTimeInterval: 0.016, repeats: true) { _ in
+                Task { @MainActor in
+                    maskTimer += rectangleSpeed
+                }
+            }
+        }
+        .onDisappear {
+            timer?.invalidate()
+            timer = nil
+        }
+
     }
 }
 
