7c72a56945f5aab789325e3b60e1f466c6804767
Anton Novoselov <nantog@gmail.com>
2026-01-12 23:14:44 +0000

    Add animated mesh gradient to VivaDicta title text on welcome page

--- VivaDicta/Views/Onboarding/OnboardingWelcomePage.swift
+++ VivaDicta/Views/Onboarding/OnboardingWelcomePage.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct OnboardingWelcomePage: View {
+    @State private var t: Float = 0.0
+    @State private var timer: Timer?
+
     var body: some View {
         VStack(spacing: 0) {
             Spacer()
@@ -29,7 +32,7 @@ struct OnboardingWelcomePage: View {
                 Text("VivaDicta")
                     .font(.largeTitle.weight(.bold))
                     .fontDesign(.rounded)
-                    .foregroundStyle(.blue)
+                    .foregroundStyle(meshGradient)
             }
             .padding(.bottom, 16)
 
@@ -65,6 +68,39 @@ struct OnboardingWelcomePage: View {
 
             Spacer()
         }
+        .onAppear {
+            timer = Timer.scheduledTimer(withTimeInterval: 0.02, repeats: true) { _ in
+                Task { @MainActor in
+                    t += 0.02
+                }
+            }
+        }
+        .onDisappear {
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+
+    private var meshGradient: MeshGradient {
+        MeshGradient(width: 3, height: 3, points: [
+            .init(0, 0), .init(0.5, 0), .init(1, 0),
+            [sinInRange(-0.8...(-0.2), offset: 0.439, timeScale: 0.342, t: t), sinInRange(0.3...0.7, offset: 3.42, timeScale: 0.984, t: t)],
+            [sinInRange(0.1...0.8, offset: 0.239, timeScale: 0.084, t: t), sinInRange(0.2...0.8, offset: 5.21, timeScale: 0.242, t: t)],
+            [sinInRange(1.0...1.5, offset: 0.939, timeScale: 0.084, t: t), sinInRange(0.4...0.8, offset: 0.25, timeScale: 0.642, t: t)],
+            [sinInRange(-0.8...0.0, offset: 1.439, timeScale: 0.442, t: t), sinInRange(1.4...1.9, offset: 3.42, timeScale: 0.984, t: t)],
+            [sinInRange(0.3...0.6, offset: 0.339, timeScale: 0.784, t: t), sinInRange(1.0...1.2, offset: 1.22, timeScale: 0.772, t: t)],
+            [sinInRange(1.0...1.5, offset: 0.939, timeScale: 0.056, t: t), sinInRange(1.3...1.7, offset: 0.47, timeScale: 0.342, t: t)]
+        ], colors: [
+            .blue, .purple, .indigo,
+            .cyan, .pink, .blue,
+            .purple, .indigo, .cyan
+        ])
+    }
+
+    private func sinInRange(_ range: ClosedRange<Float>, offset: Float, timeScale: Float, t: Float) -> Float {
+        let amplitude = (range.upperBound - range.lowerBound) / 2
+        let midPoint = (range.upperBound + range.lowerBound) / 2
+        return midPoint + amplitude * sin(timeScale * t + offset)
     }
 }
 
