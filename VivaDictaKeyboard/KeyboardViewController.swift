bdc7a95be5766fcf05928072327134c7f64dd024
Anton Novoselov <nantog@gmail.com>
2026-01-13 15:46:00 +0000

    Remove Activate button, show Mic button always

--- VivaDictaKeyboard/KeyboardViewController.swift
+++ VivaDictaKeyboard/KeyboardViewController.swift
@@ -81,61 +81,6 @@ class KeyboardViewController: KeyboardInputViewController {
 
 }
 
-struct ActivateButton: View {
-    @Environment(\.openURL) private var openURL
-    @State var isAnimating = false
-    
-    var borderWidth: CGFloat
-    weak var controller: KeyboardViewController?
-    
-    var body: some View {
-        Button {
-            // Build URL with hostId as query parameter
-            var urlString = "vivadicta://record-for-keyboard"
-            if let hostId = controller?.hostApplicationBundleId {
-                // URL encode the hostId to handle special characters
-                if let encodedHostId = hostId.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
-                    urlString += "?hostId=\(encodedHostId)"
-                }
-            }
-
-            if let url = URL(string: urlString) {
-                controller?.logger.logInfo("📱 Opening main app with URL: \(url.absoluteString)")
-                openURL(url)
-            }
-        } label: {
-            
-            Label("Activate", systemImage: "mic.slash")
-                .font(.system(size: 18, weight: .medium))
-                .foregroundColor(Color.primary)
-                .colorInvert()
-                .padding(.horizontal, 16)
-                .padding(.vertical, 8)
-                .background(.gray.gradient, in: .capsule(style: .continuous))
-            
-                .background {
-                    Capsule(style: .continuous)
-                    
-                        .fill(AngularGradient(colors: [.teal, .pink, .teal], center: .center, angle: .degrees(isAnimating ? 360 : 0)))
-                        .blur(radius: 10)
-                        .onAppear {
-                            withAnimation(Animation.linear(duration: 7).repeatForever(autoreverses: false)) {
-                                isAnimating = true
-                            }
-                        }
-                        .onDisappear {
-                            isAnimating = false
-                        }
-                }
-            
-                .overlay {
-                    Capsule(style: .continuous)
-                        .stroke(.black.opacity(0.5), lineWidth: borderWidth)
-                }
-        }
-    }
-}
-
 
 // MARK: - Mode Cycle Selector
 struct ModeCycleSelector: View {
@@ -224,37 +169,22 @@ struct VivaDictaKeyboardToolbarView: View {
 
             Spacer()
 
-            if dictationState.uiState == .notReady {
-                if #available(iOS 26.0, *) {
-                    ActivateButton(borderWidth: 0, controller: controller)
-                        .glassEffect(.regular.tint(.gray.opacity(1.0)).interactive())
-
-                } else {
-                    ActivateButton(borderWidth: 0.5, controller: controller)
-                }
-
+            // Always show MicButton - it handles notReady state by opening main app
+            if #available(iOS 26.0, *) {
+                MicButton(
+                    fontSize: 34,
+                    padding: 6,
+                    backgroundColor: .orange.opacity(0.5),
+                    borderWidth: 0,
+                    onTapAction: handleMic)
+                    .glassEffect(.regular.tint(.orange.opacity(1.0)).interactive())
             } else {
-
-                if #available(iOS 26.0, *) {
-                    MicButton(
-                        fontSize: 34,
-                        padding: 6,
-                        backgroundColor: .orange.opacity(0.5),
-                        borderWidth: 0,
-                        onTapAction: handleMic)
-
-                        .glassEffect(.regular.tint(.orange.opacity(1.0)).interactive())
-
-                } else {
-
-                    MicButton(
-                        fontSize: 36,
-                        padding: 0,
-                        backgroundColor: .orange,
-                        borderWidth: 0.5,
-                        onTapAction: handleMic)
-
-                }
+                MicButton(
+                    fontSize: 36,
+                    padding: 0,
+                    backgroundColor: .orange,
+                    borderWidth: 0.5,
+                    onTapAction: handleMic)
             }
         }
         .padding(.horizontal, 16)
