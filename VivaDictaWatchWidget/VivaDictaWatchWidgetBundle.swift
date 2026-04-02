e4debbd01fb77b1d340ad3a5725f57c732834272
Anton Novoselov <nantog@gmail.com>
2026-04-02 23:50:11 +0100

    Add watch widget extension target for complications and controls

---
+++ VivaDictaWatchWidget/VivaDictaWatchWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  VivaDictaWatchWidgetBundle.swift
+//  VivaDictaWatchWidget
+//
+//  Created by Anton Novoselov on 02.04.2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct VivaDictaWatchWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        VivaDictaWatchWidget()
+        VivaDictaWatchWidgetControl()
+    }
+}
