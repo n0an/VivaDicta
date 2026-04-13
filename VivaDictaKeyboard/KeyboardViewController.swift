1fd0a19ddfabd4056a98eaa8732fe11cc1fd8173
Anton Novoselov <nantog@gmail.com>
2026-04-13 22:55:28 +0100

    Fix keyboard mic gradient target membership issue

--- VivaDictaKeyboard/KeyboardViewController.swift
+++ VivaDictaKeyboard/KeyboardViewController.swift
@@ -344,7 +344,7 @@ private struct KeyboardMicButton: View {
     @ViewBuilder
     private var styledBackground: some View {
         if colorScheme == .dark {
-            AnimatedMeshGradient()
+            KeyboardAnimatedMeshGradient()
                 .mask(
                     Circle()
                         .stroke(lineWidth: 12)
@@ -368,7 +368,7 @@ private struct KeyboardMicButton: View {
                 .background(.black)
                 .clipShape(.circle)
         } else {
-            AnimatedMeshGradient2()
+            KeyboardAnimatedMeshGradient2()
                 .overlay(
                     Circle()
                         .stroke(lineWidth: 3)
@@ -387,4 +387,56 @@ private struct KeyboardMicButton: View {
                 .shadow(color: .black.opacity(0.45), radius: 8, x: 0, y: 4)
         }
     }
+}
+
+private struct KeyboardAnimatedMeshGradient: View {
+    @State private var startDate = Date.now
+
+    var body: some View {
+        TimelineView(.animation) { timeline in
+            let t = Float(timeline.date.timeIntervalSince(startDate)) * 10
+            MeshGradient(width: 3, height: 3, points: [
+                .init(0, 0), .init(0.5, 0), .init(1, 0),
+                [keyboardSinInRange(-0.8...(-0.2), offset: 0.439, timeScale: 0.342, t: t), keyboardSinInRange(0.3...0.7, offset: 3.42, timeScale: 0.984, t: t)],
+                [keyboardSinInRange(0.1...0.8, offset: 0.239, timeScale: 0.084, t: t), keyboardSinInRange(0.2...0.8, offset: 5.21, timeScale: 0.242, t: t)],
+                [keyboardSinInRange(1.0...1.5, offset: 0.939, timeScale: 0.084, t: t), keyboardSinInRange(0.4...0.8, offset: 0.25, timeScale: 0.642, t: t)],
+                [keyboardSinInRange(-0.8...0.0, offset: 1.439, timeScale: 0.442, t: t), keyboardSinInRange(1.4...1.9, offset: 3.42, timeScale: 0.984, t: t)],
+                [keyboardSinInRange(0.3...0.6, offset: 0.339, timeScale: 0.784, t: t), keyboardSinInRange(1.0...1.2, offset: 1.22, timeScale: 0.772, t: t)],
+                [keyboardSinInRange(1.0...1.5, offset: 0.939, timeScale: 0.056, t: t), keyboardSinInRange(1.3...1.7, offset: 0.47, timeScale: 0.342, t: t)]
+            ], colors: [
+                .red, .purple, .indigo,
+                .orange, .white, .blue,
+                .yellow, .black, .mint
+            ])
+        }
+    }
+}
+
+private struct KeyboardAnimatedMeshGradient2: View {
+    @State private var startDate = Date.now
+
+    var body: some View {
+        TimelineView(.animation) { timeline in
+            let t = Float(timeline.date.timeIntervalSince(startDate)) * 10
+            MeshGradient(width: 3, height: 3, points: [
+                .init(0, 0), .init(0.5, 0), .init(1, 0),
+                [keyboardSinInRange(-0.8...(-0.2), offset: 0.439, timeScale: 0.342, t: t), keyboardSinInRange(0.3...0.7, offset: 3.42, timeScale: 0.984, t: t)],
+                [keyboardSinInRange(0.1...0.8, offset: 0.239, timeScale: 0.084, t: t), keyboardSinInRange(0.2...0.8, offset: 5.21, timeScale: 0.242, t: t)],
+                [keyboardSinInRange(1.0...1.5, offset: 0.939, timeScale: 0.084, t: t), keyboardSinInRange(0.4...0.8, offset: 0.25, timeScale: 0.642, t: t)],
+                [keyboardSinInRange(-0.8...0.0, offset: 1.439, timeScale: 0.442, t: t), keyboardSinInRange(1.4...1.9, offset: 3.42, timeScale: 0.984, t: t)],
+                [keyboardSinInRange(0.3...0.6, offset: 0.339, timeScale: 0.784, t: t), keyboardSinInRange(1.0...1.2, offset: 1.22, timeScale: 0.772, t: t)],
+                [keyboardSinInRange(1.0...1.5, offset: 0.939, timeScale: 0.056, t: t), keyboardSinInRange(1.3...1.7, offset: 0.47, timeScale: 0.342, t: t)]
+            ], colors: [
+                .blue, .red, .orange,
+                .orange, .indigo, .red,
+                .cyan, .purple, .mint
+            ])
+        }
+    }
+}
+
+private func keyboardSinInRange(_ range: ClosedRange<Float>, offset: Float, timeScale: Float, t: Float) -> Float {
+    let amplitude = (range.upperBound - range.lowerBound) / 2
+    let midPoint = (range.upperBound + range.lowerBound) / 2
+    return midPoint + amplitude * sin(timeScale * t + offset)
 }
